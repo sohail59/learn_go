5379cea88f4f2867f123a56cef94ee59bcf18c51
Md Sohail <md_sohail@mcafee.com>
2021-04-29 02:55:08 -0600

    Implemented test cases in BDD style, documented the code

--- exercises/maps/main.go
+++ exercises/maps/main.go
@@ -4,10 +4,10 @@ import (
 	"strings"
 )
 
+// WordCount return the count of each word present in given 
+// string separated by one or more space
 func WordCount(s string) map[string]int {
-	words := strings.Fields(s)
-	wordCountMap := make(map[string]int)
-
+	words := strings.Fields(s) //default delimiter is 'space'
 	//count the word in the map
 	for _, word := range words {
 
--- exercises/maps/main_test.go
+++ exercises/maps/main_test.go
@@ -5,36 +5,44 @@ import (
 	"testing"
 )
 
-// table for testcases
-var TestCases = []struct {
-	str      string
-	expected map[string]int
-}{
-	{
-		str:      "My Favorite programming language is Go",
-		expected: map[string]int{"My": 1, "Favorite": 1, "programming": 1, "language": 1, "is": 1, "Go": 1},
-	},
-	{
-		str:      "tenet is read tenet backward",
-		expected: map[string]int{"tenet": 2, "is": 1, "read": 1, "backward": 1},
-	},
-	{
-		str:      "cat is lion unless said cat",
-		expected: map[string]int{"cat": 2, "unless": 1, "is": 1, "lion": 1},
-	},
-}
-
 func TestWordCount(t *testing.T) {
 
-	for _, tt := range TestCases {
+	t.Run("test empty string input", func(t *testing.T) {
+		input := ""
+		actual := WordCount(input)
+		expected := make(map[string]int, 0)
 
-		t.Run("Test values", func(t *testing.T) {
-			actual := WordCount(tt.str)
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("expected an empty map: %v", actual)
+		}
+	})
 
-			if !reflect.DeepEqual(actual, tt.expected) {
-				t.Errorf("actual %v, expected %v", actual, tt.expected)
-			}
-		})
+	t.Run("test large space between words", func(t *testing.T) {
+		input := `Hello						world`
+		actual := WordCount(input)
+		expected := map[string]int{
+			"Hello": 1,
+			"world": 1,
+		}
 
-	}
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("expected: %v does not match with actual: %v", expected, actual)
+		}
+
+	})
+
+	t.Run("test case sensitiveness", func(t *testing.T) {
+		input := "Tenet is read tenet"
+		actual := WordCount(input)
+		expected := map[string]int{
+			"Tenet": 1,
+			"is":    1,
+			"read":  1,
+			"tenet": 1,
+		}
+
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("expected: %v does not match with actual: %v", expected, actual)
+		}
+	})
 }
